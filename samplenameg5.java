856ff077d24a832c140e31a91a46aafcbc0cbb33
nvavaishnavi <nandulavaishu@gmail.com>
2025-10-21 23:22:03 -0700

    Update samplenameg5.java

--- samplenameg5.java
+++ samplenameg5.java
@@ -1,7 +1,7 @@
 public class samplenameg5{
 public static void main(String[] args){
     String name = "vaishnavi";
-    System.out.println("Hello,my name is"+name);
+    System.out.println("Hello,my name is "+name);
 }
 } 
 
